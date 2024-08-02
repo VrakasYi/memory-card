ca0efeb3c91ac089b306396d7639bbd7d0de09b7
VrakasYi <vrakasyi@outlook.com>
2024-08-02 08:00:27 +0300

    working on score

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import Score from './components/Score.jsx'
+import Cards from './components/Cards.jsx'
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
+  // const [score, setScore] = useState(0);
+  // const [bestScore, setBestScore] = useState(0);
 
   return (
     <>
       <div>
-        <div className='header'>
-
+        <div className='header'>          
+          Memory game
+          <Score />
         </div>
         <div className='card-container'>
-
+          <Cards />
         </div>
       </div>
     </>
---
+++ src/components/Score.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export default function Score() {
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+
+    
+}
