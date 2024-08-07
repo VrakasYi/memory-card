0d0ff155367c7abdf368c99a1899f57d99a60c1f
VrakasYi <vrakasyi@outlook.com>
2024-08-07 22:20:51 +0300

    list generator

--- src/components/List-Generator.jsx
+++ src/components/List-Generator.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from 'react';
 
 const ListGenerator = () => {
-  const [numbers, setNumbers] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
   const generateRandomNumbers = () => {
-    const newNumbers = Array.from({ length: 10 }, () => Math.floor(Math.random() * 826) + 1);
-    setNumbers(newNumbers);
+    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 826) + 1);
+  };
+
+  const fetchCharacterData = async (number) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${number}`);
+    const data = await response.json();
+    return { key: number, img: data.image };
+  };
+
+  const fetchCharacters = async () => {
+    const randomNumbers = generateRandomNumbers();
+    const characterPromises = randomNumbers.map(fetchCharacterData);
+    const charactersData = await Promise.all(characterPromises);
+    setCharacters(charactersData);
   };
 
   return (
     <div>
-      <button onClick={generateRandomNumbers}>Start</button>
+      <button onClick={fetchCharacters}>Start</button>
       <ul>
-        {numbers.map((number, index) => (
-          <li key={index}>{number}</li>
+        {characters.map((character, index) => (
+          <li key={index}>
+            <img src={character.img} alt={`Character ${character.key}`} />
+          </li>
         ))}
       </ul>
     </div>
